7c5a57006bff18e7494a879196f7619aee21d456
Arturo Rodriguez <arturo.rdz3000@gmail.com>
2023-02-08 23:13:14 +0000

    Added tga builder classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include "vendor/tga/tgaimage.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
-	system("pause");
+	TGAImage image(100, 100, TGAImage::RGB);
+	image.flip_vertically(); // Origin from left bottom corner
+	image.set(50, 50, TGAColor(255, 0, 0, 255));
+	image.write_tga_file("output.tga");
 
 	return 0;
 }
